99293cbab2c15eb217f2e22f752dc5b7acb36bd3
Waihon Yew <yewwaihon@gmail.com>
2022-03-26 17:37:58 +0800

    Create a prototype of navigation bar using Nav & Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/NavBar';
+
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">React-Bootrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 }
